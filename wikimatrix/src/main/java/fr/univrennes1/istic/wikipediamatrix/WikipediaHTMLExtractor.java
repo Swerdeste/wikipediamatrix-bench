72a2489573ff4428877fbc86b2e060b408fdb768
achanesam <achanesam@me.com>
2022-03-18 15:41:58 +0100

    update csv

--- wikimatrix/src/main/java/fr/univrennes1/istic/wikipediamatrix/WikipediaHTMLExtractor.java
+++ wikimatrix/src/main/java/fr/univrennes1/istic/wikipediamatrix/WikipediaHTMLExtractor.java
@@ -1,15 +1,19 @@
 package fr.univrennes1.istic.wikipediamatrix;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
+import com.opencsv.CSVWriter;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+
 
 public class WikipediaHTMLExtractor {
     public static void main( String[] args ) {
@@ -27,16 +31,17 @@ public class WikipediaHTMLExtractor {
                 Elements tdbis= Objects.requireNonNull(tds.first()).nextElementSiblings();
 
                 assert td != null;
-                String[] dataLine= new String[]{td.text()};
+                List<String> dataLine= new ArrayList<>();
                 //dataLine.add(td.text());
                 for (Element tp : tdbis){
-                    dataLine += new String[]{tp.text()};
+                    dataLine.add(tp.text());
                 }
+                String[] simpleArray = new String[ dataLine.size() ];
+                dataLine.toArray( simpleArray );
 
-
-                dataLines.add(dataLine);
-                String[] test =  new String[]{td.text()};
-                System.out.println(Arrays.toString(test));
+                dataLines.add(simpleArray);
+                CSVWriter writer = new CSVWriter(new FileWriter("c:\\test\\monitor.csv"));
+                writer.writeAll(dataLines);
             }
         } catch (IOException e) {
             e.printStackTrace();
